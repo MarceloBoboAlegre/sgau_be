ba344325ea3654f42f857c2b5677dad4f6442178
MarceloBoboAlegre <167482248+MarceloBoboAlegre@users.noreply.github.com>
2024-12-01 20:25:40 -0300

    banco de dados

--- uteis/__init__.py
+++ uteis/__init__.py
@@ -2,12 +2,12 @@ import mysql.connector
 import bcrypt
 
 
-def conectar_bd(h, u, pw, db):
+def conectar_bd():
     database = mysql.connector.connect(
-        host=h,
-        user=u,
-        password=pw,
-        database=db
+        host="137.184.193.139",
+        user="arbonize",
+        password="@Rb0n1z3.db",
+        database="sgau"
     )
     return database
 
@@ -23,7 +23,7 @@ def turnoff(cursor, database):
 
 
 def cadastro_usuario(nome, email, telefone, senha):
-    db = conectar_bd('localhost', 'root', 'Coisadenerd2431$', 'sgau')
+    db = conectar_bd()
     myc = cursor_on(db)
     igual = False
     sen = senha.encode('utf-8')
@@ -58,7 +58,7 @@ def cadastro_usuario(nome, email, telefone, senha):
 
 
 def login_usuario(nome, senha):
-    db = conectar_bd('localhost', 'root', 'Coisadenerd2431$', 'sgau')
+    db = conectar_bd()
     myc = cursor_on(db)
     sql = 'SELECT * FROM user WHERE NOME = %s'
     prc = (nome, )
@@ -81,7 +81,7 @@ def login_usuario(nome, senha):
 
 
 def cadastro_arvore(arvore):
-    db = conectar_bd('localhost', 'root', 'Coisadenerd2431$', 'sgau')
+    db = conectar_bd()
     myc = cursor_on(db)
     sql = 'INSERT INTO arvores ('
     val = []
@@ -104,7 +104,7 @@ def cadastro_arvore(arvore):
 
 
 def procurar_arvore(resposta):
-    db = conectar_bd('localhost', 'root', 'Coisadenerd2431$', 'sgau')
+    db = conectar_bd()
     myc = cursor_on(db)
     for k, v in resposta.items():
         sql = f"SELECT * FROM arvores WHERE {k.capitalize()} ='{v}'"
@@ -120,7 +120,7 @@ def procurar_arvore(resposta):
 
 
 def lista_arvores():
-    db = conectar_bd('localhost', 'root', 'Coisadenerd2431$', 'sgau')
+    db = conectar_bd()
     myc = cursor_on(db)
     sql = 'SELECT * FROM arvores'
     myc.execute(sql)
@@ -140,7 +140,7 @@ def lista_arvores():
 
 
 def marcar_mapa():
-    db = conectar_bd('localhost', 'root', 'Coisadenerd2431$', 'sgau')
+    db = conectar_bd()
     myc = cursor_on(db)
     sql = 'SELECT * FROM arvores'
     myc.execute(sql)
